eadccd1dbd02ebcde95c3213e98d8a9dccc64008
Abid Khan <abidkhan2381@gmail.com>
2025-09-20 06:31:27 +0500

    frontend

--- src/components/common/ErrorFallback.jsx
+++ src/components/common/ErrorFallback.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 export default function ErrorFallback({ error, resetErrorBoundary }) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed inset-0 flex items-center justify-center z-50 p-3">
             <div className="relative flex flex-col items-center justify-center 
                       p-6 w-80 bg-gradient-to-br from-sky-50 to-white 
                       dark:from-[#0a0f1c] dark:to-[#111827] 
